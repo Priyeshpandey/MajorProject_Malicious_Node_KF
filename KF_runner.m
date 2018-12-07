126c7664614c13a5944c4c39f22700bbbe342daa
Priyeshpandey <pandeypriyesh184@gmail.com>
2018-12-07 20:48:29 +0530

    Graph is in Output folder

--- KF_runner.m
+++ KF_runner.m
@@ -1,16 +1,21 @@
-%%Generate graph for all 9 sensors
-% for k=1:dimension(2)
-%     figure
-%     plot(measurement(:,k));
-%     hold on
-%     plot(prediction(:,k));
-%     title(k)
-%     hold off
-% end
-%Uncomment and run to generate scatter plotfor malicious node detection
-  x = (0:167)'*ones(1,9);
-figure
-hold on
-for k = 1:dimension(1)
-    scatter(x(k,:),malnode(k,:).*(1:9))
+%Generate graph for all 9 sensors
+[num,strobj,raw] = xlsread('AirQualityUCI3.xlsx','E1:M1');
+for k=1:dimension(2)
+    figure
+    plot(measurement(:,k));
+    hold on
+    plot(prediction(:,k));
+    title(strcat('Data : ',strobj(k)))
+    xlabel('Time(hr)')
+    ylabel('Concentration')
+    legend('Actual data','Kalman prediction')
+    hold off
 end
+% %Uncomment and run to generate scatter plotfor malicious node detection
+%   x = (0:167)'*ones(1,9);
+% figure
+% hold on
+% for k = 1:dimension(1)
+%     scatter(x(k,:),malnode(k,:).*(1:9),'*')
+% end
+% title('Malicious Node Detection')
